c2eff1d5b7e72c72cc722094c18fb88e82436d40
AndreaColinsR <44866486+AndreaColinsR@users.noreply.github.com>
2023-01-26 20:51:18 +0000

    Documenting functions for figure 3

--- recurrence_all_sessions.m
+++ recurrence_all_sessions.m
@@ -1,5 +1,29 @@
-function recurrence_all_sessions(sessions,Areas,threshold,Ndir,Nbins,threshold_dist,do_plot_supp)
-
+function recurrence_all_sessions(Sessions,Areas,threshold,Ndir,i_bin,threshold_dist,do_plot_supp)
+%% recurrence_all_sessions plots the recurrence plots and the histograms of recurrence for the selected recordings and bin number
+% 
+% INPUTS
+%
+% Sessions: cell array containing the names of the sessions to be analysed.
+% e.g {'MC_S1_raw.mat','MC_S2_raw.mat'}
+%
+% Areas: cell array containing the names of the areas to be analysed in
+% each session. Areas and Sessions must have the same number of elements.
+% e.g {'M1','M1'}
+% 
+% threshold: percentage of the variance to be explained by the first nPCs
+%
+% Ndir: number of direction to bin the movements
+%
+% i_bin: duration bin to be analysed
+%
+% threshold_dist: threshold of the percentil of distance(theta) for a point to be
+% considered recurrent. threshold_dist ranges from 0 to 100.
+%
+% do_plot_supp: 1- plot histograms of recurrence for all recordings
+%               0- omit the plot above
+%
+% 26/01/2023
+% Andrea Colins Rodriguez
 ndim=zeros(numel(Areas),1);
 colour_dir=hsv(Ndir);
 
@@ -19,198 +43,193 @@ colormap(flipud(colormap('gray')))
 
 for iarea=1:numel(Areas)
     Area=Areas{iarea};
-    session=sessions{iarea};
-    %time_rec=nan(Ndir,Nbins);
+    session=Sessions{iarea};
+    
     
     if do_plot_supp
         
-    if strcmp(Area,'M1')
-        j_m1=j_m1+1;
-        
-    else
-        j_PMd=j_PMd+1;
-    end
-    
-    end
-    
-    for i_bin=1:Nbins
-        
-        load(['../Output_files/PCA_' session(1:end-4) '_' Area '.mat'],'score','idx_dir','idx_duration','variance','t_from','t_upto','sigma_filter')
-        
-        ndim(iarea)=find(cumsum(variance)>threshold,1,'First');
-        score=score(idx_duration==i_bin,:);
-        idx_dir=idx_dir(idx_duration==i_bin,:);
-        recurrence=pdist2(score(:,1:ndim(iarea)),score(:,1:ndim(iarea)));
-        limit=prctile(pdist(score(:,1:ndim(iarea))),threshold_dist);
-        
-        
-        
-        if iarea==1 && i_bin==1
+        if strcmp(Area,'M1')
+            j_m1=j_m1+1;
             
-            figure(fig1)
-            subplot(1,4,1)
-            plot3(score(idx_dir==1,1),score(idx_dir==1,2),score(idx_dir==1,3),'Color',colour_dir(1,:,:))
-            hold on
-            plot3(score(find(idx_dir==1,1,'First'),1),score(find(idx_dir==1,1,'First'),2),score(find(idx_dir==1,1,'First'),3),'.','MarkerSize',12,'Color',colour_dir(1,:,:))
-            
-            xlabel('PC 1')
-            ylabel('PC 2')
-            zlabel('PC 3')
-            
-            subplot(1,4,3)
-            imagesc(flipud(recurrence<limit))
-            hold on
-            xlim([1 size(recurrence,1)])
-            ylim([1 size(recurrence,1)])
-            colormap(flipud(colormap('gray')))
-            box off
-            total_time=size(score,1);
-            for i_line=1:Ndir
-                time_line=i_line*sum(idx_dir==i_line);
-                plot([time_line time_line],[0 total_time],'r')
-                plot([0 total_time],[time_line time_line],'r')
-            end
+        else
+            j_PMd=j_PMd+1;
         end
         
-        %% Compute recurrence
-        segment_length=round((t_upto(i_bin)-t_from)*1000);
-        idx=zeros(Ndir,upto);
-        hall=zeros(Ndir,numel(0:50:segment_length)-1);
-        Edges=0:50:segment_length;
-%        midbin=Edges+50/2;
+    end
+    
+    
+    
+    load(['../Output_files/PCA_' session(1:end-4) '_' Area '.mat'],'score','idx_dir','idx_duration','variance','t_from','t_upto','sigma_filter')
+    
+    ndim(iarea)=find(cumsum(variance)>threshold,1,'First');
+    score=score(idx_duration==i_bin,:);
+    idx_dir=idx_dir(idx_duration==i_bin,:);
+    recurrence=pdist2(score(:,1:ndim(iarea)),score(:,1:ndim(iarea)));
+    limit=prctile(pdist(score(:,1:ndim(iarea))),threshold_dist);
+    
+    
+    
+    if iarea==1
         
-        for i_dir=1:Ndir
-            
-            recurrencebin=recurrence((i_dir-1)*segment_length+1:i_dir*segment_length,(i_dir-1)*segment_length+1:i_dir*segment_length)<limit;
-            idx(i_dir,:)=recurrence_time(recurrencebin,upto,2*sigma_filter);
-            
-            hall(i_dir,:)=histcounts(idx(i_dir,:),Edges,'Normalization','probability');
-
-            %To compute time of recurrene, select bin with maximum number of recurrent points
-%             [val,bin]=max(hall(i_dir,:),[],'includenan');
-%             
-%             if val~=0
-%                 time_rec(i_dir,i_bin)=midbin(bin);
-%             end
-            
-            
-            if iarea==1 && i_bin==1 && i_dir==1
-                
-                figure(fig1)
-                subplot(1,4,2)
-                hold on
-                imagesc([t_from*1000 t_upto(i_bin)*1000],[t_upto(i_bin)*1000 t_from*1000],flipud(recurrencebin))
-                colormap(flipud(colormap('gray')))
-                plot([0 0],[t_from*1000 t_upto(i_bin)*1000],'--k')
-                plot([t_from*1000 t_upto(i_bin)*1000],[0 0],'--k')
-                plot([250 250],[t_from*1000 t_upto(i_bin)*1000],'--k')
-                plot([t_from*1000 t_upto(i_bin)*1000],[250 250],'--k')
-                xlim([t_from*1000 t_upto(i_bin)*1000])
-                ylim([t_from*1000 t_upto(i_bin)*1000])
-                xlabel('Time from movement onset [ms]')
-                ylabel('Time from movement onset [ms]')
-                box off
-            end
+        figure(fig1)
+        subplot(1,4,1)
+        plot3(score(idx_dir==1,1),score(idx_dir==1,2),score(idx_dir==1,3),'Color',colour_dir(1,:,:))
+        hold on
+        plot3(score(find(idx_dir==1,1,'First'),1),score(find(idx_dir==1,1,'First'),2),score(find(idx_dir==1,1,'First'),3),'.','MarkerSize',12,'Color',colour_dir(1,:,:))
+        
+        xlabel('PC 1')
+        ylabel('PC 2')
+        zlabel('PC 3')
+        
+        subplot(1,4,3)
+        imagesc(flipud(recurrence<limit))
+        hold on
+        xlim([1 size(recurrence,1)])
+        ylim([1 size(recurrence,1)])
+        colormap(flipud(colormap('gray')))
+        box off
+        total_time=size(score,1);
+        
+        for i_line=1:Ndir
+            time_line=i_line*sum(idx_dir==i_line);
+            plot([time_line time_line],[0 total_time],'r')
+            plot([0 total_time],[time_line time_line],'r')
         end
+    end
+    
+    %% Compute recurrence
+    segment_length=round((t_upto(i_bin)-t_from)*1000);
+    idx=zeros(Ndir,upto);
+    hall=zeros(Ndir,numel(0:50:segment_length)-1);
+    Edges=0:50:segment_length;
+    
+    for i_dir=1:Ndir
         
+        recurrencebin=recurrence((i_dir-1)*segment_length+1:i_dir*segment_length,(i_dir-1)*segment_length+1:i_dir*segment_length)<limit;
+        idx(i_dir,:)=recurrence_time(recurrencebin,upto,2*sigma_filter);
         
-        
-        %% Compute proportion of recurrence points across directions
-        %         proportion=zeros(Ndir,Ndir);
-        %         for i_dir=1:Ndir
-        %             recurrence_matrix=sum(sum(recurrence((i_dir-1)*segment_length+1:i_dir*segment_length,(i_dir-1)*segment_length+1:i_dir*segment_length)<limit));
-        %
-        %             for k=1:Ndir
-        %                 recurrence_matrix_other=recurrence((i_dir-1)*segment_length+1:i_dir*segment_length,(k-1)*segment_length+1:k*segment_length)<limit;
-        %                 proportion(i_dir,k)=sum(recurrence_matrix_other(:))/recurrence_matrix;
-        %             end
-        %
-        %         end
+        hall(i_dir,:)=histcounts(idx(i_dir,:),Edges,'Normalization','probability');
         
         %% Plots !!
         
-        
-        
-        if i_bin==1
-            if do_plot_supp
-                figure(fig_supp)
-                if strcmp(Area,'M1')
-                    subplot(nrows_plot,2,(j_m1-1)*2+1)
-                else
-                    subplot(nrows_plot,2,(j_PMd-1)*2+2)
-                end
-                
-                imagesc((0:50:segment_length)+t_from*1000,1:Ndir,hall)
-                title([session(1:2) ' ' session(4:5) ' ' Area])
-                caxis([0 1])
-                colormap(flipud(colormap('gray')))
-                box off
-                
-                if j_m1==sum(strcmp(Areas,'M1'))
-                    subplot(nrows_plot,2,(j_m1-1)*2+1)
-                    xlabel('Time from movement onset [ms]')
-                end
-                if j_PMd==sum(strcmp(Areas,'PMd'))
-                    subplot(nrows_plot,2,(j_PMd-1)*2+2)
-                    xlabel('Time from movement onset [ms]')
-                end
-            end
-            % Examples figure 3
-            % M1 (MC S1)
-            if iarea==1
-                figure(fig1)
-                subplot(2,4,4)
-                hold on
-                imagesc((0:50:segment_length)+t_from*1000,1:Ndir,hall)
-                plot([0 0],[0 8.5],'--k')
-                plot([300 300],[0 8.5],'--k')
-                caxis([0 1])
-                ylim([0.5 8.5])
-                title('M1')
-                set(gca, 'YDir','reverse')
-                colormap(flipud(colormap('gray')))
-                box off
-            end
-            % PMd
-            if iarea==11
-                figure(fig1)
-                subplot(2,4,8)
-                hold on
-                imagesc((0:50:segment_length)+t_from*1000,1:Ndir,hall)
-                plot([0 0],[0 8.5],'--k')
-                plot([300 300],[0 8.5],'--k')
-                caxis([0 1])
-                ylim([0.5 8.5])
-                title('PMd')
-                set(gca, 'YDir','reverse')
-                colormap(flipud(colormap('gray')))
-                box off
-                xlabel('Time to movement onset [ms]')
-            end
+        if iarea==1 && i_dir==1
+            
+            figure(fig1)
+            subplot(1,4,2)
+            hold on
+            imagesc([t_from*1000 t_upto(i_bin)*1000],[t_upto(i_bin)*1000 t_from*1000],flipud(recurrencebin))
+            colormap(flipud(colormap('gray')))
+            plot([0 0],[t_from*1000 t_upto(i_bin)*1000],'--k')
+            plot([t_from*1000 t_upto(i_bin)*1000],[0 0],'--k')
+            plot([250 250],[t_from*1000 t_upto(i_bin)*1000],'--k')
+            plot([t_from*1000 t_upto(i_bin)*1000],[250 250],'--k')
+            xlim([t_from*1000 t_upto(i_bin)*1000])
+            ylim([t_from*1000 t_upto(i_bin)*1000])
+            xlabel('Time from movement onset [ms]')
+            ylabel('Time from movement onset [ms]')
+            box off
+        end
+    end
+    
+    
+    
+    
+    if do_plot_supp
+        figure(fig_supp)
+        if strcmp(Area,'M1')
+            subplot(nrows_plot,2,(j_m1-1)*2+1)
+        else
+            subplot(nrows_plot,2,(j_PMd-1)*2+2)
         end
         
+        imagesc((0:50:segment_length)+t_from*1000,1:Ndir,hall)
+        title([session(1:2) ' ' session(4:5) ' ' Area])
+        caxis([0 1])
+        colormap(flipud(colormap('gray')))
+        box off
         
-        
-        
+        if j_m1==sum(strcmp(Areas,'M1'))
+            subplot(nrows_plot,2,(j_m1-1)*2+1)
+            xlabel('Time from movement onset [ms]')
+        end
+        if j_PMd==sum(strcmp(Areas,'PMd'))
+            subplot(nrows_plot,2,(j_PMd-1)*2+2)
+            xlabel('Time from movement onset [ms]')
+        end
+    end
+    % Examples figure 3
+    % M1 (MC S1)
+    if iarea==1
+        figure(fig1)
+        subplot(2,4,4)
+        hold on
+        imagesc((0:50:segment_length)+t_from*1000,1:Ndir,hall)
+        plot([0 0],[0 8.5],'--k')
+        plot([300 300],[0 8.5],'--k')
+        caxis([0 1])
+        ylim([0.5 8.5])
+        title('M1')
+        set(gca, 'YDir','reverse')
+        colormap(flipud(colormap('gray')))
+        box off
+    end
+    % PMd
+    if iarea==11
+        figure(fig1)
+        subplot(2,4,8)
+        hold on
+        imagesc((0:50:segment_length)+t_from*1000,1:Ndir,hall)
+        plot([0 0],[0 8.5],'--k')
+        plot([300 300],[0 8.5],'--k')
+        caxis([0 1])
+        ylim([0.5 8.5])
+        title('PMd')
+        set(gca, 'YDir','reverse')
+        colormap(flipud(colormap('gray')))
+        box off
+        xlabel('Time to movement onset [ms]')
     end
     
 end
 end
 
-function times_idx=recurrence_time(recurrencebin,upto,sigma_filter)
-idx=nan(1,upto);
+function times_idx=recurrence_time(recurrencebin,first_points,sigma_filter)
+%% recurrence_times finds the times in recurrencebin matrix that recur to the first_points
+% 
+% INPUTS
+% recurrencebin= recurrence matrix. i.e binarised distance between points
+% in square format
+%
+% first_points= number of time bins to be tested from the start of the
+% trajectories [ms]
+% 
+% sigma_filter= time bins lower bound to be considered recurrence. If a
+% time of recurrence is less than sigma_filer, the point is considered not
+% recurrent. 
+% 
+% OUTPUTS
+%
+% times_idx= array containing the times at which the points recur. Nan
+% values indicate non-recurrence. 
+%
+% 26/01/2023
+% Andrea Colins Rodriguez
 
-for point=1:upto
-    %find last contiguous point
+
+times_idx=nan(1,first_points);
+
+for point=1:first_points
     
+    %find last contiguous point
     tmp=find(diff(recurrencebin(point:end,point))>0,1,'last');
+    
     if ~isempty(tmp)
         if tmp>=sigma_filter
-            idx(point)=tmp;
+            times_idx(point)=tmp;
         end
     end
     clear tmp
 end
 
-times_idx=idx;
+
 end
--- recurrence_region_all_sessions.m
+++ recurrence_region_all_sessions.m
@@ -1,46 +1,115 @@
-function recurrence_region_all_sessions(sessions,Areas,threshold,Ndir)
+function recurrence_region_all_sessions(Sessions,Areas,threshold,Ndir,Nbins,threshold_dist)
+%% recurrence_region_all_sessions defines a region of recurrence 
+%% for each recordings and checks how mant trajectories are inside this region at each moment in time
 
-fig2=figure;
-colormap(flipud(colormap('gray')))
+% INPUTS
+%
+% Sessions: cell array containing the names of the sessions to be analysed.
+% e.g {'MC_S1_raw.mat','MC_S2_raw.mat'}
+%
+% Areas: cell array containing the names of the areas to be analysed in
+% each session. Areas and Sessions must have the same number of elements.
+% e.g {'M1','M1'}
+% 
+% threshold: percentage of the variance to be explained by the first nPCs
+%
+% Ndir: number of direction to bin the movements
+%
+% Nbins: number of durations to bin the movements
+%
+% threshold_dist: threshold of the percentil of distance(theta) for a point to be
+% considered recurrent. threshold_dist ranges from 0 to 100.
+%
+% 26/01/2023
+% Andrea Colins Rodriguez
+
+ref_bin=2; % duration bin used as reference to define the region of recurrence
 upto=200;% number of points to analyse from the beginning of prep
+
+fig1=figure;
+colormap(flipud(colormap('gray')))
+colour_dur=plasma(Nbins);
+colour_dir=hsv(Ndir);
+
 not_coming_back=zeros(numel(Areas),1);
 not_starting_there=zeros(numel(Areas),1);
-Nbins=4;
-colour_dur=plasma(Nbins);
 
 for isession=1:numel(Areas)
     Area=Areas{isession};
-    session=sessions{isession};
+    session=Sessions{isession};
     End_recurrence=true(Nbins*Ndir,1);
     Start_recurrence=true(Nbins*Ndir,1);
-    for i_bin=[2:Nbins 1]
+    
+    for i_bin=[ref_bin:Nbins 1:ref_bin-1]
         
         
         load(['../Output_files/PCA_' session(1:end-4) '_' Area '.mat'],'score','idx_dir','idx_duration','variance','t_from','t_upto')
-
-       % load(['scores_LDS_diff_duration_newfilter_' session '_' Area '.mat'],'score','idx_dir','idx_duration','variance','t_1','t_2')
+        
         ndim=find(cumsum(variance)>threshold,1,'First');
-        if i_bin==2
-             if isession==1
-                 do_example_plot=1;
-             else
-                 do_example_plot=0;
-             end
-            [limit,center]=define_recurrence_region_v3(score(idx_duration==i_bin,1:ndim),idx_dir(idx_duration==i_bin),do_example_plot);
+        
+        %% Define region of recurrence for the recording
+        if i_bin==ref_bin
 
+            [limit,center]=define_recurrence_region(score(idx_duration==i_bin,1:ndim),idx_dir(idx_duration==i_bin),threshold_dist);
+            
+            if isession==1
+                min_all=min(min(score(:,[1 2])));
+                max_all=max(max(score(:,[1 2])));
+                [X,Y,Z] = sphere;
+                
+                subplot(2,2,1)
+                colormap(colormap('white')*0.5)
+                mesh(X.*limit+center(1),Y.*limit+center(2),Z.*limit+center(3),'FaceAlpha','0.5')
+                
+                hold on
+                xlim([min_all max_all])
+                ylim([min_all max_all])
+                zlim([min_all max_all])
+                xlabel('PC 1')
+                ylabel('PC 2')
+                zlabel('PC 3')
+                
+                subplot(2,2,3)
+                colormap(colormap('white')*0.5)
+                mesh(X.*limit+center(2),Y.*limit+center(3),Z.*limit+center(4),'FaceAlpha','0.5')
+                
+                hold on
+                xlim([min_all max_all])
+                ylim([min_all max_all])
+                zlim([min_all max_all])
+                xlabel('PC 2')
+                ylabel('PC 3')
+                zlabel('PC 4')
+                
+                for i_dir=1:Ndir
+                    subplot(2,2,1)
+                    plot3(score(idx_dir==i_dir & idx_duration==i_bin,1),score(idx_dir==i_dir & idx_duration==i_bin,2),score(idx_dir==i_dir & idx_duration==i_bin,3),'Color',colour_dir(i_dir,:),'LineWidth',1)
+                    hold on
+                    
+                    subplot(2,2,3)
+                    plot3(score(idx_dir==i_dir & idx_duration==i_bin,2),score(idx_dir==i_dir & idx_duration==i_bin,3),score(idx_dir==i_dir & idx_duration==i_bin,4),'Color',colour_dir(i_dir,:),'LineWidth',1)
+                    hold on
+                end
+            end
         end
         
         %% compute recurrence time
         segment_length=round((t_upto(i_bin)-t_from)*1000);
-        Its=zeros(segment_length,Ndir);
+        
+        Is_in_Rec=zeros(segment_length,Ndir);
+        
         for i_dir=1:Ndir
-            %Its(:,i_dir)=IsInRecRegion(score(idx_dir(idx_duration==i_bin)==i_dir,1:ndim),region);
-            Its(:,i_dir)=IsInRecRegion2(score(idx_dir==i_dir & idx_duration==i_bin,1:ndim),[limit,center]);
-            Start_recurrence(Ndir*(i_bin-1)+i_dir)=sum(Its(1:upto,i_dir),1)>0;
-            End_recurrence(Ndir*(i_bin-1)+i_dir)=sum(Its(end-upto:end,i_dir),1)>0;
+            % Check if points of traj(i_dir,i_bin) are in the region of
+            % recurrence
+            Is_in_Rec(:,i_dir)=IsInRecRegion(score(idx_dir==i_dir & idx_duration==i_bin,1:ndim),[limit,center]);
+            
+            %Check if the points at the start and end are in the region of
+            %recurrence
+            Start_recurrence(Ndir*(i_bin-1)+i_dir)=sum(Is_in_Rec(1:upto,i_dir),1)>0;
+            End_recurrence(Ndir*(i_bin-1)+i_dir)=sum(Is_in_Rec(end-upto:end,i_dir),1)>0;
         end
         
-        figure(fig2)
+        figure(fig1)
         if strcmp(Areas{isession},'M1')
             subplot(2,2,2)
             hold on
@@ -48,12 +117,12 @@ for isession=1:numel(Areas)
             subplot(2,2,4)
             hold on
         end
-        plot((1:size(Its,1))+t_from*1000,sum(Its,2),'Color',colour_dur(i_bin,:))
+        
+        plot((1:segment_length)+t_from*1000,sum(Is_in_Rec,2),'Color',colour_dur(i_bin,:))
         
         
-        clear Its
     end
-    %% Compute number of points at the beginning and end. Or just if it gets there? both?
+    %% Compute number of points at the beginning and end.
     
     not_coming_back(isession,1)=sum(End_recurrence<1)/numel(End_recurrence);
     not_starting_there(isession,1)=sum(Start_recurrence<1)/numel(End_recurrence);
@@ -61,8 +130,7 @@ for isession=1:numel(Areas)
     clear Start_recurrence End_recurrence
 end
 
-M1=strcmp(Areas,'M1');
-figure(fig2)
+figure(fig1)
 subplot(2,2,2)
 title('M1')
 box off
@@ -72,74 +140,50 @@ xlabel('Time to movement onset [ms]')
 ylabel('Number of trajectories in the recurrence region')
 box off
 
-[mean(1-not_starting_there(M1)) mean(1-not_starting_there(~M1))]
-[mean(1-not_coming_back(M1)) mean(1-not_coming_back(~M1))]
+%[mean(1-not_starting_there(M1)) mean(1-not_starting_there(~M1))]
+%[mean(1-not_coming_back(M1)) mean(1-not_coming_back(~M1))]
 
 end
 
 
-function [limit,center]=define_recurrence_region_v3(score,idx_dir,do_plot)
-%this version uses the threshold to define a region
-rec_length=1;
-Ndir=max(idx_dir);
-colour_dir=hsv(Ndir);
-all_points_beginning=zeros(rec_length*Ndir,size(score,2));
-threshold=10;
-limit=prctile(pdist(score),threshold);
+function [radius,center]=define_recurrence_region(score,idx_dir,threshold_dist)
+%% define_recurrence_region defines a region of recurrence as a hypersphere around the startpoints of the trajectories
+%
+% INPUTS
+% score: neural trajectories for the selected reference duration 
+% 
+% idx_dir: array indicating the direction bin corresponding to each point
+% in score
+%
+% threshold_dist: threshold of the percentil of distance(theta) for a point to be
+% considered recurrent. threshold_dist ranges from 0 to 100.
+%
+% OUTPUTS
+% 
+% radius: radius of the recurrence region
+% 
+% center: center of the recurrence region
+%
+% 26/01/2023
+% Andrea Colins Rodriguez
 
+Ndir=max(idx_dir);
+
+rec_length=1;
+
+all_points_beginning=zeros(rec_length*Ndir,size(score,2));
+
+radius=prctile(pdist(score),threshold_dist);
 
 for i_dir=1:Ndir
-      
     all_points_beginning((i_dir-1)*rec_length+1:i_dir*rec_length,:)=score(find(idx_dir==i_dir,rec_length,'First'),:);
-    if do_plot
-        subplot(2,2,1)
-        plot3(score(idx_dir==i_dir,1),score(idx_dir==i_dir,2),score(idx_dir==i_dir,3),'Color',colour_dir(i_dir,:),'LineWidth',1)
-        hold on
-        plot3(all_points_beginning((i_dir-1)*rec_length+1:i_dir*rec_length,1),all_points_beginning((i_dir-1)*rec_length+1:i_dir*rec_length,2),all_points_beginning((i_dir-1)*rec_length+1:i_dir*rec_length,3),'Color',colour_dir(i_dir,:)/2,'LineWidth',3)
-        
-         subplot(2,2,3)
-        plot3(score(idx_dir==i_dir,2),score(idx_dir==i_dir,3),score(idx_dir==i_dir,4),'Color',colour_dir(i_dir,:),'LineWidth',1)
-        hold on
-        plot3(all_points_beginning((i_dir-1)*rec_length+1:i_dir*rec_length,2),all_points_beginning((i_dir-1)*rec_length+1:i_dir*rec_length,3),all_points_beginning((i_dir-1)*rec_length+1:i_dir*rec_length,3),'Color',colour_dir(i_dir,:)/2,'LineWidth',3)
-
-    end
 end
 
 center=mean(all_points_beginning);
-if do_plot
-     min_all=min(min(score(:,[1 2])));
-    max_all=max(max(score(:,[1 2])));
-    [X,Y,Z] = sphere;
-    
-    subplot(2,2,1)
-    colormap(colormap('white')*0.5)
-    mesh(X.*limit+center(1),Y.*limit+center(2),Z.*limit+center(3),'FaceAlpha','0.5')
-    
-    hold on
-    xlim([min_all max_all])
-    ylim([min_all max_all])
-    zlim([min_all max_all])
-    xlabel('PC 1')
-    ylabel('PC 2')
-    zlabel('PC 3')
-    
-    subplot(2,2,3)
-    colormap(colormap('white')*0.5)
-    mesh(X.*limit+center(2),Y.*limit+center(3),Z.*limit+center(4),'FaceAlpha','0.5')
-    
-    hold on
-    xlim([min_all max_all])
-    ylim([min_all max_all])
-    zlim([min_all max_all])
-    xlabel('PC 2')
-    ylabel('PC 3')
-    zlabel('PC 4')
-end
-
 end
 
 
-function Its=IsInRecRegion2(score,region)
+function Its=IsInRecRegion(score,region)
 limit=region(1);
 center=region(2:end);
 
